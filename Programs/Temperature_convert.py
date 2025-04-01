c5728eed9ca2a6797cd19c788fb4e4d4f1df5afa
unknown <gauravkathayat12@gmail.com>
2025-04-01 23:07:17 +0545

    Temperature converter adding a menu

--- Programs/Temperature_convert.py
+++ Programs/Temperature_convert.py
@@ -1,3 +1,15 @@
-C= int(input("Enter temperature in celcius:"))
-F= (C*9/5)+32
-print(F)
+for x  in range(331):
+    i = int(input("choose 0 for celcius or 1 for Farenheit"))
+    if i==0:
+        C= int(input("Enter temperature in celcius:"))
+        F=(C*9/5)+32
+        print(F)
+    elif i==1:
+        F= int(input("Enter temperature in Farenheit:"))
+        C=(F-32)*5/9
+        print(C)
+    else:
+        x=330
+        print("Invalid choice")
+        
+
