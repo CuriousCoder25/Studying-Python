6d038f4577ab7bab91cf407f6f84fa8506d42545
unknown <gauravkathayat12@gmail.com>
2025-04-01 23:35:03 +0545

    added exit button

--- Programs/Temperature_convert.py
+++ Programs/Temperature_convert.py
@@ -1,5 +1,5 @@
 for x  in range(331):
-    i = int(input("choose 0 for celcius or 1 for Farenheit"))
+    i = int(input("choose 0 for celcius or 1 for Farenheit or 2 for exit"))
     if i==0:
         C= int(input("Enter temperature in celcius:"))
         F=(C*9/5)+32
@@ -8,8 +8,11 @@ for x  in range(331):
         F= int(input("Enter temperature in Farenheit:"))
         C=(F-32)*5/9
         print(C)
+    elif i==2:
+        if i==2:
+         break
     else:
-        x=330
         print("Invalid choice")
+        
         
 
