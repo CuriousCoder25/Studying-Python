88b9de4ea0a9a3d2dc074faca8751c2cf97565af
unknown <gauravkathayat12@gmail.com>
2025-04-02 01:11:20 +0545

    FizzBuzz

--- Programs/fizzbuzz.py
+++ Programs/fizzbuzz.py
@@ -1,10 +1,14 @@
-x=1
+
 for x in range(1,51):
-    if x%3==0:
+    if x%3==0 and x%5==0:
+        print("FizzBuzz")
+    elif x%3==0:
         print("Fizz")
+        
     elif x%5==0:
         print("Buzz")
-    elif x%3==0 and x%5==0:
-        print("FizzBuzz")
+        
+    
+     
 
        
