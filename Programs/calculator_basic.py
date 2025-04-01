988b547deea2bf88e41579ccaea14f8ca60de18f
unknown <gauravkathayat12@gmail.com>
2025-04-02 01:01:24 +0545

    basic calculator

---
+++ Programs/calculator_basic.py
@@ -0,0 +1,17 @@
+#basic calculator +-/*
+print("CALCULATOR APP")
+print("\n1. Add\n2. Subtract \n3. Divide\n4. Multiply")
+x=int(input("\nCHOOSE ONE OPERATION:"))
+a=int(input("Enter Number A:"))
+b=int(input("Enter Number B:"))
+
+if x==1:
+    print(a+b)
+elif x==2:
+    print(a-b)
+elif x==3:
+    print(a/b)
+elif x==4:
+    print(a*b)
+
+
